3f3cce61129f818b10bea89e88d5da4fabe2778a
hieu1999210 <hieupro1999210@gmail.com>
2020-06-12 18:54:37 +0700

    training resnet

--- source/train.sh
+++ source/train.sh
@@ -1,7 +1,7 @@
-# CUDA_VISIBLE_DEVICES=0 python train.py \
-    # --config "/home/hieu123/alpha_zero_mp/configs/exp1.yaml" \
+CUDA_VISIBLE_DEVICES=0 python train.py \
+    --config "/home/hieu123/alpha_zero_mp/configs/exp4_hust.yaml" \
     # --resume
 
-CUDA_VISIBLE_DEVICES=0 python train.py \
-    --config "/home/ad/Documents/source/configs/exp2_local.yaml" \
-    --resume
+# CUDA_VISIBLE_DEVICES=0 python train.py \
+    # --config "/home/ad/Documents/source/configs/exp2_local.yaml" \
+    # --resume
