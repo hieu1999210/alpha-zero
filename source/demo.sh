ea0f7eebbcd88e5355749a6915ebe87eb5a14bf4
hieu1999210 <hieupro1999210@gmail.com>
2020-06-14 14:46:48 +0700

    minor change

--- source/demo.sh
+++ source/demo.sh
@@ -1,3 +1,2 @@
 CUDA_VISIBLE_DEVICE=0 python demo.py \
     --config "/mnt/DATA/learning_stuffs/uni/20192/artificial_intelligence/project/alpha-zero/source/configs/exp2_local_demo.yaml" \
-    --load "/mnt/DATA/learning_stuffs/uni/20192/artificial_intelligence/project/checkpoints/exp2_local_iter_037_epoch_010_p_loss_0.5376_v_loss_0.1458.pth"
